440c3820b5c5ff4d54ab89fad5228fbddbe3252d
saix511 <saadirfan41@gmail.com>
2023-07-19 19:18:37 +0500

    .

--- src/components/Providers/NextAuthProvider.tsx
+++ src/components/Providers/NextAuthProvider.tsx
@@ -5,7 +5,6 @@ import { SessionProvider } from 'next-auth/react'
 interface Props {
   children: ReactNode
 }
-
 const NextAuthProvider = ({ children }: Props) => {
   return <SessionProvider>{children}</SessionProvider>
 }
